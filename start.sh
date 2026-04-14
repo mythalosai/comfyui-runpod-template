fc66ca99672a427c1fbf593288f5ac34d55527d9
mythalosai <mythalosai@gmail.com>
2026-04-14 12:01:09 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 cd /workspace/ComfyUI
 
 echo "📦 Setting up environment..."
@@ -25,7 +27,7 @@ if [ ! -f models/checkpoints/qwen_2.5_vl_7b_fp8_scaled.safetensors ]; then
 fi
 
 if [ ! -f models/vae/qwen_image_vae.safetensors ]; then
-    echo "Downloading VAE..."
+    echo "Downloading Qwen VAE..."
     wget -c -O models/vae/qwen_image_vae.safetensors \
     https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/vae/qwen_image_vae.safetensors
 fi
@@ -34,6 +36,28 @@ if [ ! -f models/checkpoints/qwen_image_edit_2511.safetensors ]; then
     echo "Downloading Qwen Image Edit..."
     wget -c -O models/checkpoints/qwen_image_edit_2511.safetensors \
     https://huggingface.co/Comfy-Org/Qwen-Image-Edit_ComfyUI/resolve/main/split_files/diffusion_models/qwen_image_edit_2511_bf16.safetensors
+fi
+
+# =========================
+# 🔽 Z IMAGE TURBO
+# =========================
+
+if [ ! -f models/checkpoints/z_image_turbo.safetensors ]; then
+    echo "Downloading Z Image Turbo..."
+    wget -c -O models/checkpoints/z_image_turbo.safetensors \
+    https://huggingface.co/Comfy-Org/z_image_turbo/resolve/main/split_files/diffusion_models/z_image_turbo_bf16.safetensors
+fi
+
+if [ ! -f models/text_encoders/qwen_3_4b.safetensors ]; then
+    echo "Downloading Qwen 3.4B encoder..."
+    wget -c -O models/text_encoders/qwen_3_4b.safetensors \
+    https://huggingface.co/Comfy-Org/z_image_turbo/resolve/main/split_files/text_encoders/qwen_3_4b.safetensors
+fi
+
+if [ ! -f models/vae/zimage_vae.safetensors ]; then
+    echo "Downloading Z Image VAE..."
+    wget -c -O models/vae/zimage_vae.safetensors \
+    https://huggingface.co/Comfy-Org/z_image_turbo/resolve/main/split_files/vae/ae.safetensors
 fi
 
 # =========================
@@ -47,25 +71,59 @@ if [ ! -f models/checkpoints/ltx_2.3_22b_fp8.safetensors ]; then
 fi
 
 if [ ! -f models/upscale_models/ltx_upscaler_x2.safetensors ]; then
-    echo "Downloading Upscaler..."
+    echo "Downloading LTX Upscaler..."
     wget -c -O models/upscale_models/ltx_upscaler_x2.safetensors \
     https://huggingface.co/Lightricks/LTX-2.3/resolve/main/ltx-2.3-spatial-upscaler-x2-1.0.safetensors
 fi
 
-if [ ! -f models/loras/ltx_2.3_lora_384.safetensors ]; then
-    echo "Downloading LORA..."
-    wget -c -O models/loras/ltx_2.3_lora_384.safetensors \
-    https://huggingface.co/Lightricks/LTX-2.3/resolve/main/ltx-2.3-22b-distilled-lora-384.safetensors
-fi
-
 # =========================
-# 🔽 GEMMA (TEXT ENCODER)
+# 🔽 GEMMA
 # =========================
 
 if [ ! -f models/text_encoders/gemma_3_12b_fp4.safetensors ]; then
     echo "Downloading Gemma..."
     wget -c -O models/text_encoders/gemma_3_12b_fp4.safetensors \
     https://huggingface.co/Comfy-Org/ltx-2/resolve/main/split_files/text_encoders/gemma_3_12B_it_fp4_mixed.safetensors
+fi
+
+# =========================
+# 🔽 LORAS
+# =========================
+
+if [ ! -f models/loras/qwen_lightning.safetensors ]; then
+    echo "Downloading Qwen Lightning LoRA..."
+    wget -c -O models/loras/qwen_lightning.safetensors \
+    https://huggingface.co/lightx2v/Qwen-Image-Edit-2511-Lightning/resolve/main/Qwen-Image-Edit-2511-Lightning-4steps-V1.0-bf16.safetensors
+fi
+
+if [ ! -f models/loras/qwen_multiangle.safetensors ]; then
+    echo "Downloading Multi-Angle LoRA..."
+    wget -c -O models/loras/qwen_multiangle.safetensors \
+    https://huggingface.co/fal/Qwen-Image-Edit-2511-Multiple-Angles-LoRA/resolve/main/qwen-image-edit-2511-multiple-angles-lora.safetensors
+fi
+
+if [ ! -f models/upscale_models/clear_reality.pth ]; then
+    echo "Downloading ClearReality Upscaler..."
+    wget -c -O models/upscale_models/clear_reality.pth \
+    https://huggingface.co/skbhadra/ClearRealityV1/resolve/bc01e27b38eec683dc6e3161dd56069c78e015ac/4x-ClearRealityV1.pth
+fi
+
+if [ ! -f models/loras/fruitisland_qwen.safetensors ]; then
+    echo "Downloading FruitIsland Qwen LoRA..."
+    wget -c -O models/loras/fruitisland_qwen.safetensors \
+    https://huggingface.co/Mythalos/FruitIlsland/resolve/main/fruitisland_qwenimage.safetensors
+fi
+
+if [ ! -f models/loras/fruitisland_zimage.safetensors ]; then
+    echo "Downloading FruitIsland ZImage LoRA..."
+    wget -c -O models/loras/fruitisland_zimage.safetensors \
+    https://huggingface.co/Mythalos/FruitIlsland/resolve/main/fruitisland_zimage.safetensors
+fi
+
+if [ ! -f models/loras/ltx_id_lora.safetensors ]; then
+    echo "Downloading LTX ID LoRA..."
+    wget -c -O models/loras/ltx_id_lora.safetensors \
+    https://huggingface.co/AviadDahan/LTX-2.3-ID-LoRA-CelebVHQ-3K/resolve/main/lora_weights.safetensors
 fi
 
 # =========================
